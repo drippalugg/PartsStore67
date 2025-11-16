6731b7c5c996e6114a7488b432bc4ac867c1c5d3
drippalugg <shinerear314@gmail.com>
2025-11-16 15:32:13 +0300

    Initial commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.partsstore {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.base;
-    requires com.google.gson;
-    requires java.net.http;
-}
